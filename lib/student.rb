8d8cbdd30cdd2f85cf83aa386a4d12e04c5939e9
ktochow1 <ktochow1@binghamton.edu>
2019-03-12 17:04:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -83,13 +83,13 @@ class Student
   end
   
   def self.first_X_students_in_grade_10(number)
-    sql = %{
+    sql = <<-SQL
       SELECT * 
       FROM students 
       WHERE grade = 10
         ORDER BY students.id
       LIMIT = ?
-      }
+      SQL
       
     DB[:conn].execute(sql, number).map do |row|
       self.new_from_db(row)
